c4d0ec6e94f7ef203c67cfd2896633d3189928ee
gksqhfdl12 <gksqhfdl12@gmail.com>
2024-06-12 11:21:46 +0900

    edit

---
+++ jstl/src/test/java/com/example/jstl/JstlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jstl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
